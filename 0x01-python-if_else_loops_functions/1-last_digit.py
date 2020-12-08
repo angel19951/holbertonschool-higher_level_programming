cd8684540ce28a3442470e71dae7c02ba6dc533c
angel19951 <2384@holbertonschool.com>
2020-12-08 12:05:50 -0400

    Fixing code for checks

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -8,14 +8,14 @@ else:
     last = number % 10
 
 if last > 5:
-    print(("Last digit of {:d}"
-           " is {:d} and "
+    print(("Last digit of {:d} "
+           "is {:d} and "
            "is greater than 5").format(number, last))
 elif last == 0:
-    print(("Last digit of {:d}"
-           " is {:d} and "
+    print(("Last digit of {:d} "
+           "is {:d} and "
            "is 0").format(number, last))
 elif last < 6 and last > 0:
-    print(("Last digit of {:d}"
-           " is {:d} and "
+    print(("Last digit of {:d} "
+           "is {:d} and "
            "is less than 6 and not 0").format(number, last))
