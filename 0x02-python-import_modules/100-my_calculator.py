93fd2e7f5be94964c571cc694c3190613411e70a
angel19951 <2384@holbertonschool.com>
2020-12-10 22:54:36 -0400

    Fixing code for checks

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     from sys import argv, exit
-    from calculator_1 import add, sub, mul, div
+    from calculator_1 import *
     length = len(argv)
     if length != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
