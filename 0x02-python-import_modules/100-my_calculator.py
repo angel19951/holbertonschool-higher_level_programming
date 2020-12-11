16773cb2de187f82c4074aeb66887a8232597a39
angel19951 <2384@holbertonschool.com>
2020-12-10 22:51:33 -0400

    Operations calculator | import & module | Python

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from sys import argv, exit
+    from calculator_1 import add, sub, mul, div
+    length = len(argv)
+    if length != 4:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        exit(1)
+
+    fir_num = int(argv[1])
+    sec_num = int(argv[3])
+
+    operation = argv[2]
+    if operation == "+":
+        result = add(fir_num, sec_num)
+    elif operation == "-":
+        result = sub(fir_num, sec_num)
+    elif operation == "*":
+        result = mul(fir_num, sec_num)
+    elif operation == "/":
+        rersult = div(fir_num, sec_num)
+    else:
+        print("unkown operator. avialable operators: +, - * and /")
+        exit(1)
+    print("{:d} {} {:d} = {:d}".format(fir_num, operation, sec_num, result))
