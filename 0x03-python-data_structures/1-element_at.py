bde917d21b1209c63f9b33aa08602ee067e37f61
angel19951 <2384@holbertonschool.com>
2020-12-15 13:22:14 -0400

    Fixing code for checks

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,7 +1,14 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
+    count = 0
+    if idx < 0:
+        return None
+    for r in my_list:
+        count += 1
+    if idx > count:
+        return None
     for i in my_list:
         if i == idx:
-            element = my_list[idx]
+            element = my_list[i]
 
     return element
