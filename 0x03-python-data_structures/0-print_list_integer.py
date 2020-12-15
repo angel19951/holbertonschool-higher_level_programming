d41d86da6324dadbc5a0800889921799fd1cd2c0
angel19951 <2384@holbertonschool.com>
2020-12-15 13:13:15 -0400

    Fixing code for checks

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
     for elements in my_list:
-        print("{}".format(my_list[elements - 1]))
+        print("{:d}".format(my_list[elements - 1]))
