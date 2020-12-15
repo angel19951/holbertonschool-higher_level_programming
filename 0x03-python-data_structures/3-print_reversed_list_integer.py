12a5821d8b4b004f6b5872e21426ad1103cf374a
angel19951 <2384@holbertonschool.com>
2020-12-15 14:12:51 -0400

    Fixing code for checks

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,8 +2,6 @@
 
 
 def print_reversed_list_integer(my_list=[]):
-    length = len(my_list)
 
-    for i in my_list[::-1]:
-        if i != -1:
+    for i in reversed(my_list):
             print("{:d}".format(i))
