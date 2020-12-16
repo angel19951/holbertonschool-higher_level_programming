69098361c789d4f5a2500f524b3a991094477cde
angel19951 <2384@holbertonschool.com>
2020-12-16 09:47:30 -0400

    Fixing code for checks

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,7 +3,9 @@
 
 def print_matrix_integer(matrix=[[]]):
 
-    for row in matrix:
-        for col in row:
-            print("{:d} ".format(col), end='')
+    for row in range(len(matrix)):
+        for col in range(len(matrix[row])):
+            print("{:d}".format(matrix[row][col]), end='')
+            if col != len(matrix[row]) - 1:
+                print('{}'.format(" "), end='')
         print()
