c0ad5b54872e110fb7e0d8d6e28e916bae48dca5
angel19951 <2384@holbertonschool.com>
2020-12-17 23:17:29 -0400

    Delete key from a dictionary by value | Python - More Data Structures: Set, Dictionary | Python

---
+++ 0x04-python-more_data_structures/102-complex_delete.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+
+def complex_delete(a_dictionary, value):
+    for key, val in dict(a_dictionary).items():
+        if val == value:
+            a_dictionary.pop(key)
+    return a_dictionary
