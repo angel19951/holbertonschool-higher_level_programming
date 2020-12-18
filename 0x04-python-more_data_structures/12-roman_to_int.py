69d32138eaefe956ccab9c3f9554aab8791b2325
angel19951 <2384@holbertonschool.com>
2020-12-17 22:13:34 -0400

    Fixing pep8 complaint

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,16 +2,16 @@
 
 
 def roman_to_int(roman_string):
-    roman_round = {"I":1, "V":5, "X":10, "L": 50, "C":100, "D":500, "M": 1000}
-    roman_special = {"IV":4, "IX":9, "XL":40, "XC":90, "CD":400, "CM":900}
+    roman_r = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+    roman_s = {"IV": 4, "IX": 9, "XL": 40, "XC": 90, "CD": 400, "CM": 900}
     i = 0
     num = 0
 
     while i < len(roman_string):
-        if i + 1 < len(roman_string) and roman_string[i:i+2] in roman_special:
-            num += roman_special[roman_string[i:i+2]]
+        if i + 1 < len(roman_string) and roman_string[i:i+2] in roman_s:
+            num += roman_s[roman_string[i:i+2]]
             i += 2
         else:
-            num += roman_round[roman_string[i]]
+            num += roman_r[roman_string[i]]
             i += 1
     return num
