f4b7d91d2526f880cece8f94d083a4de37ba8847
angel19951 <2384@holbertonschool.com>
2020-12-17 22:20:00 -0400

    Fixing code for checks

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -8,7 +8,7 @@ def roman_to_int(roman_string):
     num = 0
     if len(roman_string) == 0:
         return 0
-    if roman_string.isnumeric():
+    if roman_string.isalpha() is False:
         return 0
     while i < len(roman_string):
         if i + 1 < len(roman_string) and roman_string[i:i+2] in roman_s:
