b91e345e624de4ba975a3073218853908eb1bac9
angel19951 <2384@holbertonschool.com>
2021-01-04 16:12:16 -0400

    Fixing code for checks

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,11 +2,12 @@
 
 
 def safe_print_list(my_list=[], x=0):
-    for i in range(0, x):
+    count = 0
+    while count != x:
         try:
-            print("{}".format(my_list[i]), end='')
-            i = i + 1
+            print("{}".format(my_list[count]), end='')
+            count += 1
         except:
             break
     print()
-    return i
+    return count
