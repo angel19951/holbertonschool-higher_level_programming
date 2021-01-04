0977c846470adf72a60a8a54476dc10d799d9d34
angel19951 <2384@holbertonschool.com>
2021-01-04 16:30:21 -0400

    Print only integers | Exceptions | Python

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+
+def safe_print_integer(value):
+    try:
+        num = int(value)
+        print("{:d}".format(num))
+        return True
+    except:
+        return False
