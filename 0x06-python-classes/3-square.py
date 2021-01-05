e001cd97df31eea2660b231f665f1986e6038d82
angel19951 <2384@holbertonschool.com>
2021-01-05 17:22:54 -0400

    Define a square and return area | Python classes | Python

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""
+This module contains a class
+with the attribure size validating their entrys
+"""
+
+
+class Square:
+    """This is a sqaure class."""
+    def __init__(self, size=0):
+        """
+        Takes in size and validates it is a number and >= 0
+        Args:
+        size (int): size of the square, must be int
+        """
+        if isinstance(size, int) is not True:
+            raise TypeError("size must be an integer")
+        if int(size) < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """
+        Return the multiplication of size by itself
+        """
+        return int(self.__size) * int(self.__size)
