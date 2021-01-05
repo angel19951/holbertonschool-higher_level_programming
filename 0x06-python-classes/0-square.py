dba44cc052890906bf1baddbe681b1c4b8e9d660
angel19951 <2384@holbertonschool.com>
2021-01-05 17:05:58 -0400

    fixing code for checks

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
+"""This module contains an empty square class."""
 
 
-class Square():
-    """This is a square"""
+class Square:
+    """This is a square class."""
     pass
---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+class Square:
+    """This is a square"""
+
+    def __init__(self, size=0):
+        """
+        Takes in an int for instance attribute size
+        Args:
+        size (int): size of the square
+        """
+        self.__size=size #:giving attribute size a value
