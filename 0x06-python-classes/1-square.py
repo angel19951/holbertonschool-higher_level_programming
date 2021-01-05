c6bc026452f6937e03d0918ec50cc3d60fd74505
angel19951 <2384@holbertonschool.com>
2021-01-05 17:14:35 -0400

    Fixing pep8 complaint

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 """This module contains a square class with the attribute size"""
+
+
 class Square:
     """This is a square"""
-
     def __init__(self, size=0):
         """
         Takes in an int for instance attribute size
