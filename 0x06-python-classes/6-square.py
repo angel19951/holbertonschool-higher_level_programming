a70d926908e0d4c85fc9105afd8db3f71912dbc3
angel19951 <2384@holbertonschool.com>
2021-01-05 19:34:13 -0400

    Define and print a sqaure with given position | Python classes | Python

---
+++ 0x06-python-classes/6-square.py
@@ -0,0 +1,72 @@
+#!/usr/bin/python3
+"""
+This module contains a class
+with the attribure size validating their entrys
+"""
+
+
+class Square:
+    """This is a sqaure class."""
+
+    def __init__(self, size=0, position=(0, 0)):
+        """
+        Takes in size variable
+        Attributes:
+        size (int): size of the square, must be int
+        position (tuple): position of the square
+        """
+        self.__size = size
+        self.__position = position
+
+    def area(self):
+        """
+        Return the multiplication of size by itself
+        """
+        return self.__size ** 2
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """
+        Defines the value of size
+        and validates if is a num and >= 0
+        Attributes:
+        value (int): size of the square, must be int
+        """
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    """
+    Defines the value of position
+    and validates if is a tuple and >= 0
+    value (tuple): position of the sqaure, must be int
+    """
+    def position(self, value):
+        if isinstance(value, tuple) is not True or value[0] < 0:
+            raise TypeError("position must be of 2 positive tuples")
+        elif value[1] < 0:
+            raise TypeError("position must be of 2 positibe tuples")
+
+    def my_print(self):
+        """
+        Prints to stdout a are square of the given size
+        """
+        if self.__size == 0:
+            print()
+        else:
+            print("\n" * self.__position[1], end="")
+            for row in range(0, self.__size):
+                print(" " * self.__position[0], end="")
+                print("#" * self.__size)
