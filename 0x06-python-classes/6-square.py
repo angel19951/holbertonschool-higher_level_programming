f0c9a00f5f675e2a4ae49f5e11ee7d139316605b
angel19951 <2384@holbertonschool.com>
2021-01-05 20:00:54 -0400

    Fixing code for checks

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -15,8 +15,8 @@ class Square:
         size (int): size of the square, must be int
         position (tuple): position of the square
         """
-        self.__size = size
-        self.__position = position
+        self.size = size
+        self.position = position
 
     def area(self):
         """
