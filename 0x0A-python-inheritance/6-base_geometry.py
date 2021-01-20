673c295ff7c593bf140e4bc28d4ac7788ae7390c
angel19951 <2384@holbertonschool.com>
2021-01-20 01:38:25 +0000

    Class BaseGeometry and raise exception | Inheritance | Python

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+This module contains an empty class named BaseGeometry
+"""
+
+
+class BaseGeometry:
+    """
+    BaseGeometry empty class
+    """
+    def area(self):
+        raise Exception("area() is not implemented")
