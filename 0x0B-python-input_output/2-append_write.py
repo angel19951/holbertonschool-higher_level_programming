5c1640c56508c1d2a5d3b82837ca0f00e1b0c850
angel19951 <2384@holbertonschool.com>
2021-01-20 21:53:48 +0000

    Fixing code for checks

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -7,5 +7,4 @@ This module appends to a .txt file
 def append_write(filename="", text=""):
     with open(filename, 'a', encoding='UTF8') as f:
         f.write(text)
-
-    return len(text)
+        return len(text)
