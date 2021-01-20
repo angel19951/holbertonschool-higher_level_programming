c86893cceca939eadb1988b431237d6182a9de52
angel19951 <2384@holbertonschool.com>
2021-01-20 21:32:14 +0000

    Fixing code for checks

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -5,7 +5,12 @@ This module writes to a .txt file
 
 
 def write_file(filename="", text=""):
+    """
+    Opens and writes to a file
+    Args:
+    filename (str): name of the file to write in
+    text (str): string to write in the file
+    """
     with open(filename, 'w', encoding="UTF8") as f:
         f.write(text)
-
-    return len(text)
+        return len(text)
