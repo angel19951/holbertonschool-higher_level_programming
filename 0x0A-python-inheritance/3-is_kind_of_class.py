6bed69960836271b7ad55d8ec10880fb565f22ae
angel19951 <2384@holbertonschool.com>
2021-01-20 03:11:29 +0000

    Fixing code for checks

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -2,13 +2,13 @@
 """
 This module verifies if a object is an instance,
 or if the object is an instance of a class that inherited
-from a specific class.
+from a specific class
 """
 
 
 def is_kind_of_class(obj, a_class):
     """
-    Returns True is obj is an instance or if is an instance 
+    Returns True is obj is an instance or if is an instance
     of a class inherited from a specific class
     """
     return isinstance(obj, a_class)
