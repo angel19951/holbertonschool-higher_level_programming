25bfdfdd75066088c093d29a51fe336b88a02aa4
angel19951 <2384@holbertonschool.com>
2021-01-21 14:03:36 +0000

    Fixing code for checks

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -12,13 +12,13 @@ def pascal_triangle(n):
     if n <= 0:
         triangle = [[]]
         return triangle
-    else:
-        triangle = [[1]]
 
-        for count in range(1, n):
-            t = [1]
-            for move in range(1, count):
-                t.append(triangle[count - 1][move - 1] + triangle[count - 1][move])
-            t.append(1)
-            triangle.append(t)
+    triangle = [[1]]
+
+    for count in range(1, n):
+        t = [1]
+        for move in range(1, count):
+            t.append(triangle[count - 1][move - 1] + triangle[count - 1][move])
+        t.append(1)
+        triangle.append(t)
     return triangle
