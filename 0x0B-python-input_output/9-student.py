9aee9c2fd911eff69fe5df97120e584d31436cdc
angel19951 <2384@holbertonschool.com>
2021-01-21 00:36:18 +0000

    Student class and dict retriever | Input, Output | Python

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""
+This module contains a class named Student
+and intances first_name, last_name, age
+"""
+
+
+class Student:
+    """
+    Class named Student
+    Instances:
+    first_name (str): name of the student
+    last_name (str): last name of the student
+    age (int): age of the student
+    """
+    def __init__(self, first_name, last_name, age):
+        """
+        Initializes a Student object
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self):
+        """
+        Returns dict with simple data struct
+        """
+        return self.__dict__
+
+
