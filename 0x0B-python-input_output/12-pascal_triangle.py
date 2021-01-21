422ccd9f0d7b3d618ab59ad43b0fd9b2d88bdef9
angel19951 <2384@holbertonschool.com>
2021-01-21 14:02:18 +0000

    Adding edge case n = 0

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -9,12 +9,16 @@ def pascal_triangle(n):
     """
     Returns list of list rep of Pascal's triangle
     """
-    triangle = [[1]]
+    if n <= 0:
+        triangle = [[]]
+        return triangle
+    else:
+        triangle = [[1]]
 
-    for count in range(1, n):
-        t = [1]
-        for move in range(1, count):
-            t.append(triangle[count - 1][move - 1] + triangle[count - 1][move])
-        t.append(1)
-        triangle.append(t)
+        for count in range(1, n):
+            t = [1]
+            for move in range(1, count):
+                t.append(triangle[count - 1][move - 1] + triangle[count - 1][move])
+            t.append(1)
+            triangle.append(t)
     return triangle
