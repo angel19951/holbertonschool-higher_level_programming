5d3a779a99332b010eb2cf20b7a2681c2b1a39a7
angel19951 <2384@holbertonschool.com>
2021-01-21 00:39:41 +0000

    Fixing

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -16,6 +16,9 @@ class Student:
     def __init__(self, first_name, last_name, age):
         """
         Initializes a Student object
+        first_name (str): name of the student
+        last_name (str): last name of the student
+        age (int): age of the student
         """
         self.first_name = first_name
         self.last_name = last_name
