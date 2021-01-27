d67a0e22906ba9ee6ab5d1bf3326b7d0b55d7727
angel19951 <2384@holbertonschool.com>
2021-01-27 14:02:35 +0000

    Fixing code for checks

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -41,9 +41,9 @@ class Square(Rectangle):
         Assings and validates the value of size
         """
         if type(value) is not int:
-            raise TypeError("width must be an integer")
+            raise TypeError("size must be an integer")
         elif value < 0:
-            raise ValueError("width must be >= 0")
+            raise ValueError("size must be >= 0")
         else:
             self.width = value
 
