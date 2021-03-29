1adb42486ead363133cf1985cdc89ad4e6f24400
angel19951 <2384@holbertonschool.com>
2021-03-29 03:46:55 +0000

    Adding read me

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -3,7 +3,7 @@
 exports.esrever = function (list) {
   let lgt = list.length - 1;
 
-  for (let num = 0; num <= list.length / 2; num++, lgt--) {
+  for (let num = 0; num < list.length / 2; num++, lgt--) {
     list[num] = list[lgt];
     list[lgt] = list[num];
   }
