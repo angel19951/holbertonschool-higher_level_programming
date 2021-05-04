9539289ccd12750261351e8c3674c558b6a9d26b
Angel <2384@holbertonschool.com>
2021-05-04 20:38:42 +0000

    Handling error

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -2,5 +2,7 @@
 const request = require('request');
 
 request(process.argv[2], function (error, response) {
-  console.log('code:', response.statusCode);
+  if (error) {
+    console.log('code:', response.statusCode);
+  }
 });
