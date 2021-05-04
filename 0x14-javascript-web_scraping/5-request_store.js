90d86dc196f519783ee45f5be8aca72c9f7b42e7
Angel <2384@holbertonschool.com>
2021-05-04 23:12:42 +0000

    Correctly handling error

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -3,9 +3,13 @@ const request = require('request');
 const fs = require('fs');
 
 request(process.argv[2], function (error, response, body) {
-  fs.writeFile(process.argv[3], body, 'utf8', function (error) {
-    if (error) {
-      console.log(error);
-    }
-  });
+  if (error) {
+    console.log(error);
+  } else {
+    fs.writeFile(process.argv[3], body, 'utf8', function (error) {
+      if (error) {
+        console.log(error);
+      }
+    });
+  }
 });
