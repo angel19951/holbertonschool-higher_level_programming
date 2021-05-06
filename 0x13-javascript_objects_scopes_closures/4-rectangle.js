c1f7e8109759ad3017c7bbf59d388a870f45537b
Angel <2384@holbertonschool.com>
2021-05-06 17:42:17 +0000

    Fixing code for checkers

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-class Rectangle {
+module.exports = class Rectangle {
   constructor (w, h) {
     if (w > 0 && h > 0) {
       this.width = w;
@@ -15,14 +15,12 @@ class Rectangle {
   }
 
   rotate () {
-    this.height = this.width;
     this.width = this.height;
+    this.height = this.width;
   }
 
   double () {
     this.width *= 2;
     this.height *= 2;
   }
-}
-
-module.exports = Rectangle;
+};
