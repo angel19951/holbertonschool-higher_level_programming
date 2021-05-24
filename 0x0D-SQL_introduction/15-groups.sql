6d3a0334c128ddbbed806784cb72c763fef22c09
Angel Gonzalez <2384@holbertonschool.com>
2021-05-24 21:35:56 +0000

    Adding task 15

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,6 +1,6 @@
 -- Lists number of recors with the same score in a table
 -- Ordered by descending count
-SELECT 'score', COUNT(*) AS 'number'
-FROM 'second_table'
-GROUP BY 'score'
-ORDER BY 'number' DESC;
+SELECT score, COUNT('score') AS number
+FROM second_table
+GROUP BY score
+ORDER BY number DESC;
