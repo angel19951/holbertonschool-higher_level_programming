d9c376f8c1b53fa1627961854a2b726063a73072
Angel Gonzalez <2384@holbertonschool.com>
2021-05-24 21:33:53 +0000

    Fixing parsing

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,5 +1,6 @@
 -- Lists number of recors with the same score in a table
 -- Ordered by descending count
-SELECT 'score', COUNT(*) AS 'number' FROM 'second_table'
+SELECT 'score', COUNT(*) AS 'number'
+FROM 'second_table'
 GROUP BY 'score'
 ORDER BY 'number' DESC;
